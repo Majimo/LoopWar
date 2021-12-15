e53781d1d9b22be845d2448483139b8a80f2b37f
Majimo <pierre.fervel@gmail.com>
2021-12-15 18:10:01 +0100

    Add walking sound effect

--- Scripts/CharacterFollow.gd
+++ Scripts/CharacterFollow.gd
@@ -1,11 +1,16 @@
 extends PathFollow2D
 
-export var character_speed = 300
+export var character_speed = 100
+
+onready var audio_stream = $AudioStreamPlayer2D
 
 var first_lap = true
 
 
 #### METHODS ####
+
+func _ready():
+	play_walk_sound()
 
 func _process(delta):
 	set_offset(get_offset() + character_speed * delta)
@@ -16,3 +21,8 @@ func _process(delta):
 	# Detection of a complete lap
 	if (get_offset() - character_speed * delta) < 0 && !first_lap:
 		EVENTS.emit_signal("lap_finished")
+
+func play_walk_sound():
+	audio_stream.play()
+	yield(get_tree().create_timer(0.7), "timeout")
+	play_walk_sound()
